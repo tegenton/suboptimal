245a10248da051e8e8db05decd8fe11ead4f7ce4
Robert Herlihy <29720977+BroccoliRaab@users.noreply.github.com>
2022-01-19 11:18:07 -0500

    Make the c version actually work

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,28 +1,27 @@
+#include <stdio.h>
 #define MINIMUM 0
 #define MAXIMUM 100
 
 
-inline int range(int *a, int max){
-  if (*a<=max){
-     return ++*a;
-  }
-  return 0;
+static inline int range(int *a, int max){
+  return ++*a<=max;
 }
 
-inline int isdivisible(int *reg, int div){
+static inline int isdivisible(int *reg, int div){
   return (*reg%div==0);
 }
 
-inline int divisors(int * reg, div1, div2, div3){
+static inline int divisors(int * reg, int div1, int div2, int div3){
  if ( isdivisible(reg, div1)){
    return 0;
- }else if (isdivisible(reg, div2){
-   return 1;
- }else if (isdivisible(reg, div3){
-   return 2;
- }else{
-   return 3;
  }
+ if (isdivisible(reg, div2)){
+   return 1;
+ }
+ if (isdivisible(reg, div3)){
+   return 2;
+ }
+ return 3;
 }
 
 int main(){
@@ -30,26 +29,28 @@ int main(){
  char buzz[] = "buzz";
  char fizzbuzz[] = "fizzbuzz";
  char str[4];
+ int res;
   
- void *out;
- *out = MINIMUM;
+ res = MINIMUM;
+ void * out = &res;
  while (range(out, MAXIMUM)){
    switch(divisors(out, 15, 3, 5)){
      case 0:
        out = fizzbuzz;
        break;
-     case: 1:
+     case 1:
        out = fizz;
        break;
      case 2:
        out = fizzbuzz;
        break;
      case 3:
-       sprintf(str,"%d",*out);
+       sprintf(str,"%d",*(int *)out);
        out = str;
    }
    
    puts(out);
+   out = &res;
  }
   
 }
